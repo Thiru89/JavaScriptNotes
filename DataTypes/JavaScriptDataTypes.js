c86bbb29c50cdd04256aad553872685b5be34393
Thirunavukkarasu Narayanan <47706372+Thiru89@users.noreply.github.com>
2023-04-24 07:27:51 -0400

    Create JavaScriptDataTypes.js

---
+++ DataTypes/JavaScriptDataTypes.js
@@ -0,0 +1,19 @@
+//Primitive Data Types
+var name = "Thiru"
+console.log(name);
+var lname;
+console.log(lname);
+var a =100
+var b =2.5
+var c="Thiru"
+var arr = [1,2,3]
+var ud = undefined
+var arr1 = [4,5]
+console.log(a, typeof(a))
+console.log(b, typeof(b))
+console.log(c, typeof(c))
+console.log(arr, typeof(arr))
+console.log(ud, typeof(ud))
+console.log(arr+[,4,5])
+console.log(typeof(arr+[,4,5]))
+console.log(arr+arr1)
